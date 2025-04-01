e3f3873068cfcf6d69f770f5ddf300b695683c57
suyanpython <suyantian@yahoo.com>
2025-04-01 22:13:19 +0200

    simplified with JpaRepo instead of DAO

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.luv2code.springboot.demo.mycoolapp.dao;
 
 import com.luv2code.springboot.demo.mycoolapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
